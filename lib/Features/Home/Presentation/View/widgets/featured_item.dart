2c5b12e2d51411ef96503dc3f178109c2ec1cc5b
Abdelrahman Mahmoud <abdelrhmanmahmo0ud@gmail.com>
2025-02-23 01:30:30 +0200

    finish display Feature Books part 2 add cashed Network image

--- lib/Features/Home/Presentation/View/widgets/featured_item.dart
+++ lib/Features/Home/Presentation/View/widgets/featured_item.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: deprecated_member_use
 
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedItem extends StatelessWidget {
@@ -11,17 +13,29 @@ class FeaturedItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: DecorationImage(
-              image: NetworkImage(
-                imageUrl,
-              ),
-              fit: BoxFit.cover,
-            )),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(
+          imageUrl: imageUrl,
+          fit: BoxFit.fill,
+          placeholder: (context, url) => DotsIndicator(
+            dotsCount: 4,
+            animate: true,
+            decorator: DotsDecorator(
+              size: const Size.square(9.0),
+              activeSize: const Size(18.0, 9.0),
+              activeShape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0)),
+            ),
+          ),
+          errorWidget: (context, url, error) => const Icon(
+            Icons.error,
+            size: 80,
+            color: Colors.redAccent,
+          ),
+        ),
       ),
     );
   }
