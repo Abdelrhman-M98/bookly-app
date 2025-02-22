c8f3c399043a3658ce014ab70b988a8b2b2676bf
Abdelrahman Mahmoud <abdelrhmanmahmo0ud@gmail.com>
2025-02-22 03:34:17 +0200

    finish home repo

--- lib/Features/Home/Data/repos/home_repo_implementation.dart
+++ lib/Features/Home/Data/repos/home_repo_implementation.dart
@@ -35,8 +35,26 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject:Programming');
+      List<BookModel> books = [];
+      for (var book in data['items']) {
+        books.add(BookModel.fromJson(book));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          errorMessage: e.toString(),
+        ),
+      );
+    }
   }
 }
